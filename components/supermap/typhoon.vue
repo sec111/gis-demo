c779f12785a3d01c906fa8c91d2379953d42dbe1
lizenghuan <li_zh2@ecidi.com>
2019-12-30 09:50:02 +0800

    update pupup tooltip

---
+++ components/supermap/typhoon.vue
@@ -0,0 +1,41 @@
+<template>
+</template>
+<script>
+const colorLine = [
+  { name: '中国', color: '#E73B52' },
+  { name: '中国台湾', color: '#C52ECF' },
+  { name: '日本', color: '#2FB63A' },
+  { name: '美国', color: '#31A3B1' },
+  { name: '中国香港', color: '#E75F3B' },
+];
+
+export default {
+  inject: ['getMap', 'getUI', 'getLayer'],
+  data() {
+    return {
+      tempPoint();
+    };
+  },
+  methods: {
+    init() {
+      this.map = this.getMap();
+      this.ui = this.getUI();
+      this.layer = this.getLayer();
+    },
+    add() {
+      
+    },
+    // 用户自定义颜色
+    genPolyline(data) {
+      const { colorLine, ui } = this;
+      const colorItem = colorLine.find(c => c.name === data.tm);
+      const option = { 
+        color: colorItem.color,
+        weight: 2,
+        dashArray: '5'
+      }
+      return ui.addPolyline(data, option);
+    }
+  }
+};
+</script>
--- pages/test.vue
+++ pages/test.vue
@@ -1,29 +1,38 @@
 <template>
   <div>
-    aaa
     <div class="map-test">
       <label>切换底图：</label>
       <select name="public-choice" v-model="basemapName" @change="changeBasemap">                                        
           <option v-for="mapUrl in mapUrlList" :key="mapUrl.name" :value="mapUrl.name">{{mapUrl.label}}</option>                                    
       </select>
+      <br/>
       <button @click="addSingleMarker()">添加单个点</button>
       <button @click="clearGroup('singlePoint')">删除单个点</button>
-      <button @click="addMarker()">添加批量点</button>
+
+      <!-- <button @click="addSinglePulseMarker()">添加单个闪烁点</button>
+      <button @click="clearGroup('singlePulsePoint')">删除单个闪烁点</button> -->
+
+      <button @click="addMultiMarker()">添加批量点</button>
       <button @click="clearGroup('multiPoint')">删除批量点</button>
-      <button @click="console.log(1)">添加线</button>
-      <button @click="console.log(1)">添加面</button>
-      <button @click="console.log(1)">添加标注</button>
-      <button @click="console.log(1)">添加范围</button>
-      <button @click="console.log(1)">添加popup</button>
 
-      <button @click="console.log(1)">绑定popup</button>
+      <button @click="addUserPolygon()">添加面</button>
+      <button @click="clearGroup('polygon')">删除面</button>
 
+      <button @click="addMarkerWithTooltip()">添加tooltip</button>
+      <button @click="clearGroup('tooltip')">删除tooltip</button>
+
+      <button @click="addMarkerWithPopup()">添加popup</button>
+      <button @click="clearGroup('popup')">删除popup</button>
+
+      <button @click="addEvent()">监听事件点</button>
+      <button @click="clearGroup('eventMarker')">删除事件点</button>
+      <br/>
       <button @click="console.log(1)">添加台风</button>
       <button @click="console.log(1)">取消台风</button>
+
       <button @click="console.log(1)">激活测量</button>
       <button @click="console.log(1)">取消测量</button>
-      <button @click="console.log(1)">添加点</button>
-      
+      <br/>
       <div>
         <supermap :ref="id" :id="id" :option="option" />
       </div>
@@ -80,7 +89,9 @@ export default {
           ]
         }
       ],
+      
       basemapName: 'tianditu',
+      tempIcon: {}
     };
   },
   methods: {
@@ -92,39 +103,148 @@ export default {
       const basemapOption = mapUrlList.find(m => m.name === basemapName);
       addTiandituBaseMap(basemapName, basemapOption.url);
     },
-    // 数据对应个性化icon
+
+    // 数据映射--个性化icon
     genIcon(data) {
-      const iconUrl = data.damType > 2 ? damJson : panoIcon;
+      const iconUrl = data.damType > 2 ? dzIconThirty : panoIcon;
       return {
         iconUrl,
         iconSize: [14, 14]
-      };   
+      };
     },
-    // 添加标注点
-    addUserMarker(markerData, genIcon, groupName) {
-      const { supermap } = this;
-      const { addMarker } = supermap.ui;
-      const { genLayerGroup, getLayerGroup } = supermap.layer;
 
-      genLayerGroup(groupName);
-      const group = getLayerGroup(groupName);
-      addMarker(markerData, genIcon, group);
-    },
     // 示例： 添加单个标注点
     addSingleMarker() {
-      const { genIcon, addUserMarker } = this;
+      const { genIcon, supermap } = this;
+      const { addMarker } = supermap.ui;
+      const { genLayerGroup } = supermap.layer;
+
       const groupName = 'singlePoint';
+      const group = genLayerGroup(groupName);
+
       const markerData = damJson[5];
-      addUserMarker(markerData, genIcon, groupName);
+      addMarker(markerData, group, { iconFunc: genIcon });
     },
     
     // 示例： 添加多个标注点
-    addMarker() {
-      const { genIcon, addUserMarker } = this;
+    addMultiMarker() {
+      const { genIcon, supermap } = this;
+      const { addMarker } = supermap.ui;
+      const { genLayerGroup } = supermap.layer;
+
       const groupName = 'multiPoint';
+      const group = genLayerGroup(groupName);
+
       const markerData = damJson;
-      addUserMarker(markerData, genIcon, groupName);
+      addMarker(markerData, group, { iconFunc: genIcon });
     },
+
+    // 示例：添加多个带tooltip的点
+    addMarkerWithTooltip() {
+      const { genIcon, supermap, renderTooltip } = this;
+      const { addMarker } = supermap.ui;
+      const { genLayerGroup } = supermap.layer;
+
+      const groupName = 'tooltip';
+      const group = genLayerGroup(groupName);
+
+      const markerData = damJson;
+      const tooltipOption = { opacity: 0.8, direction: 'top', className: 'tooltip', permanent: true };
+
+      addMarker(markerData, group, { iconFunc: genIcon, tooltipOption, renderTooltip });
+    },
+    renderTooltip(data) { // return <String|HTMLElement|Function|Tooltip>
+      const fontSize = data.damType > 2 ? 20 : 16;
+      const color = data.damType > 2 ? 'blue' : 'red';
+      const style = 'font-size: ' + fontSize + 'px; color: ' + color;
+
+      // const content = data.damName;
+      
+      const content = '<span style="' + style + '">' + data.damName + '</span>';
+
+      return this.common.str2htmlElement(content);
+    },
+
+    // 示例：添加多个带popup的点
+    addMarkerWithPopup() {
+      const { genIcon, supermap, renderPopup } = this;
+      const { addMarker } = supermap.ui;
+      const { genLayerGroup } = supermap.layer;
+
+      const groupName = 'popup';
+      const group = genLayerGroup(groupName);
+
+      const markerData = damJson;
+      const popupOption = { closeOnEscapeKey: 'auto', closeButton: false, keepInView: true, isOpen: true };
+
+      addMarker(markerData, group, { iconFunc: genIcon, popupOption, renderPopup });
+    },
+
+    renderPopup(data) { // return <String|HTMLElement|Function|Tooltip>
+      const fontSize = data.damType > 2 ? 20 : 16;
+      const color = data.damType > 2 ? 'blue' : 'red';
+      const style = 'font-size: ' + fontSize + 'px; color: ' + color;
+
+      // const content = data.damName;
+      
+      const content = '<span style="' + style + '">' + data.damName + '</span>';
+
+      return this.common.str2htmlElement(content);
+    },
+
+    // 示例：添加多个点击闪烁的点
+    addEvent() {
+      const { genIcon, supermap, renderPopup, focusOnMarker, focusOutMarker } = this;
+      const { addMarker } = supermap.ui;
+      const { genLayerGroup } = supermap.layer;
+
+      const groupName = 'eventMarker';
+      const group = genLayerGroup(groupName);
+      
+      const markerData = damJson;
+      const event = {
+        'popupopen': focusOnMarker,
+        'popupclose': focusOutMarker
+      };
+
+      const popupOption = { closeOnEscapeKey: 'auto', closeButton: false, keepInView: true, isOpen: true };
+
+      addMarker(markerData, group, { iconFunc: genIcon, popupOption, renderPopup, event });
+    },
+    // 设置闪烁
+    focusOnMarker(e) {
+      const marker = e.target;
+      this.tempIcon = marker.options.icon;
+      this.supermap.ui.setActiveIcon(marker);
+    },
+    // 取消闪烁
+    focusOutMarker(e) {
+      const marker = e.target;
+      marker.setIcon(this.tempIcon);
+    },
+
+    // 示例：添加区域
+    addUserPolygon() {
+      const { supermap } = this;
+      const { addPolygon } = supermap.ui;
+      const { genLayerGroup, getLayerGroup } = supermap.layer;
+
+      const groupName = 'polygon';
+      genLayerGroup(groupName);
+      const group = getLayerGroup(groupName);
+
+      const polygonData = [
+        [[37, 114.05],[41, 111.03],[41, 112.05],[37, 112.04]],
+        [[41, 115.05],[40, 112.03],[36, 112.05],[35, 112.04]],
+      ];
+      polygonData.forEach((p, i) => {
+        const color = i === 1 ? '#ffff00' : '#ff00ff';
+        const option = { fillColor: color, fillOpacity: 0.5, stroke: false };
+        addPolygon(p, option, group);
+      });
+    },
+
+
 
     // 示例： 关闭图层组
     closeMarker(groupName) {
@@ -140,11 +260,9 @@ export default {
     },
     // 示例： 移除图层组
     clearGroup(groupName) {
-      const { clearLayers, getLayerGroup } = this.supermap.layer;
-      const group = getLayerGroup(groupName);
-      group && clearLayers(group);
+      const group = this.supermap.layer.getLayerGroup(groupName);
+      this.supermap.layer.clearLayers(group);
     }
-
   },
   mounted() {
     this.supermap = this.$refs[this.id];
